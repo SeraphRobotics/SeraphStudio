a892d73e2b90896298541edfbf941ca40f46a7e1
= <jeffreyilipton@gmail.com>
2012-12-11 00:49:06 -0500

    removed datecheck

--- software/projects/apps/FabStudio/mainwindow.cpp
+++ software/projects/apps/FabStudio/mainwindow.cpp
@@ -76,15 +76,15 @@ MainWindow::MainWindow(QWidget *parent)
 {
 
 
-
+/*
   QDateTime curdt = QDateTime::currentDateTime();
-  QDateTime settime = QDateTime(QDate::fromString("12:12:2012", "d':'MM':'yyyy"),QTime(0,0));
+  QDateTime settime = QDateTime(QDate::fromString("12:12:2013", "d':'MM':'yyyy"),QTime(0,0));
 
   if (curdt>settime){
     QMessageBox::information(this->centralWidget(),"info","Your version has expired. please contact Seraph Robotics");
     QTimer::singleShot(0,this,SLOT(close()));
   }
-
+*/
 
   // Initialize the main window's settings
   ui->setupUi(this);
